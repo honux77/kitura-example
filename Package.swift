6e6adcfeea4711993b5cd7f08b0e363ead6a9e00
Hoyoung Jung <honux77@gmail.com>
2017-03-28 15:54:23 +0900

    Change package name

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "myKituraProject",
+    name: "kitura-example",
 	 dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6)
     ])
